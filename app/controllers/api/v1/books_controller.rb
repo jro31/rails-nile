f98fdb382ca5008f604d5725c438b5d030a2500e
Jethro Williams <jethrowilliams@outlook.com>
2021-11-30 11:11:04 +0000

    Use activerecord 'limit' and 'offset' methods to allow pagination
    With specs

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class BooksController < ApplicationController
       def index
-        books = Book.all
+        books = Book.limit(params[:limit]).offset(params[:offset])
 
         render json: BooksRepresenter.new(books).as_json
       end
