cb8e1d5baa75e120474db6a5a2eb05a3961e55a3
Jethro Williams <jethrowilliams@outlook.com>
2021-11-30 18:03:47 +0000

    Rename the authentication token service 'call' method to 'encode', and handle a decode error (when the request is missing an authorization header)

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,9 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::BooksController, type: :controller do
-  it 'has a max limit of 100' do
-    expect(Book).to receive(:limit).with(100).and_call_original
+  describe 'GET index' do
+    it 'has a max limit of 100' do
+      expect(Book).to receive(:limit).with(100).and_call_original
 
-    get :index, params: { limit: 999 }
+      get :index, params: { limit: 999 }
+    end
+  end
+
+  describe 'POST create' do
+    context 'missing authorization header' do
+      it 'returns a 401' do
+        post :create, params: {}
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe 'DELETE destroy' do
+    context 'missing authorization header' do
+      it 'returns a 401' do
+        delete :destroy, params: { id: 1 }
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
   end
 end
